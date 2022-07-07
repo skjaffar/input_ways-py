f2a34c75f86c42b2762c03cd705394c6738fd7e6
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-07 23:32:52 +0530

    Add files via upload

---
+++ user_intreface.py
@@ -0,0 +1,56 @@
+# a= input("enter your name:")
+# print('my name is',a)
+# print("my name is",a,sep="_")
+# print("my name is",int(a))
+# print("my name is",float(a))
+# print("my name is"+' '+str(a))
+# print("my name is"+' '+a)
+
+# a=int(input('enter your phone number:'))
+# print("your phone number is",a,sep='_ ')
+# print("your phone number is",int(a))
+# print("your phone number is",float(a))
+# print("your phone number is",str(a))
+
+# a=float(input('enter your BMI is:'))
+# print('enter your BMI is',a)
+# print('enter your BMI is',a,sep=" ")
+# print("enter your BIM  is",int(a))
+# print("enter your BIM  is",float(a))
+# print("enter your BIM  is",str(a))
+
+
+# name=input("enter your name:")
+# age=int(input("enter your age:"))
+# per=float(input("enter your graduation percentage:"))
+# print(name,age,per)
+# print(per,name,age)
+# print(name,age,per)
+
+# print("your name is",name,"and age is",age,\
+# "graduation percentage is",per)
+
+# print("your name is {} and age is {}.  \
+# graduation percentage is {}".format(name,age,per))
+
+
+# print("your name is {0} and age is {1}.  \
+# graduation percentage is {2}".format(name,age,per))
+
+
+# print(f"your name is {name} and age is {age}. \
+# graduation percentage is {per}")
+
+# a,b=[float(x) for x in input('enter any two numbers').split()]
+# print(a+b)
+
+
+# import math
+# a=[float(x) for x in input().split(',')]
+# print(math.prod(a))
+
+
+
+import math
+a=[float(x) for x in input().split(',')]
+print(math.prod(a))
